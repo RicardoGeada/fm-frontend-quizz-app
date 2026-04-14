e10e234f3bf923a7480d934ee47d45a6f81927c8
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2026-04-14 08:39:22 +0200

    feat: add start page layout with basic structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Start } from './pages/start/start';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: Start}
+];
---
+++ src/app/pages/start/start.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start',
+  imports: [],
+  templateUrl: './start.html',
+  styleUrl: './start.scss',
+})
+export class Start {}
